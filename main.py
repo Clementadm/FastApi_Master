e769883510585829e99822e680fe52a61be9f042
Clementadm <clement_demoussac@hotmail.fr>
2023-11-29 14:49:16 +0100

    home page; begin off header

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi import Response
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse, status_code=200)
+async def home(request: Request) -> Response:
+    data = {
+        "titreSound": "GoToSound",
+        "titrePicture" : "GotoPicture",
+        "titreText" : "GoToText",
+        "titreVideo":"GoToVideo"
+    }
+    return templates.TemplateResponse("home.html", {"request": request, "data": data})
+
+
+@app.get("/test", response_class=HTMLResponse, status_code=200)
+async def home(request: Request) -> Response:
+    data = {
+        "aaaaa": "eee",
+        "bb" : "ggg"
+    }
+    return templates.TemplateResponse("header.html", {"request": request, "data": data})
 
 
 @app.get("/items/{id}", response_class=HTMLResponse)
