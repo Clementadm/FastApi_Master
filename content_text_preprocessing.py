2847bb8d6841cdd44d1086f3628b475a585f7544
BaptisteRdt <rodetbaptiste@gmail.com>
2024-02-15 11:28:38 +0100

    [upd] content_text_preprocessing

--- content_text_preprocessing.py
+++ content_text_preprocessing.py
@@ -1,6 +1,16 @@
 import re
 
 def clean_text(text: str) -> str:
-    clean_text = re.sub(r'\n{2,}', '\n', text)
+    text = re.sub(r'\n{2,}', '\n', text)
+    sentences = text.split('\n')
+
+    clean_text = ''
+    singular_word_list = []
+
+    for sentence in sentences:
+        if ' ' not in sentence:
+            singular_word_list.append(sentence)
+        else:
+            clean_text += f'{sentence}\n'
 
     return clean_text
