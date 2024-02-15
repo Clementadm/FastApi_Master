fc8f6fa47798395759cc3665062879e1dc03966f
BaptisteRdt <rodetbaptiste@gmail.com>
2024-02-15 11:03:52 +0100

    [upd] requirements, router, scraping_function

--- router.py
+++ router.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 # from Text.html import checkbox_Text, checkbox_Url
 from scraping_function import scrap_url_user
+from content_text_preprocessing import clean_text
 
 def configuration_route(app: FastAPI):
     app.mount(
@@ -48,11 +49,14 @@ def configuration_route(app: FastAPI):
 
         # Vérification de si l'utilisateur à remplie du text ou une URL
         if RadioToChooseTEXTorURL=="HaveChooseText":
-            print("1-HaveChooseText", RadioToChooseTEXTorURL)
+            print(f'1-{RadioToChooseTEXTorURL}')
+            print(text_input)
         if RadioToChooseTEXTorURL=="HaveChooseURL":
             # scraper le text du body de l'URL
-            print("2-HaveChooseURL", RadioToChooseTEXTorURL)
-            print(scrap_url_user(text_input))
+            print(f'2-{RadioToChooseTEXTorURL}')
+            content = scrap_url_user(text_input)
+            modified_content = clean_text(content)
+            print(modified_content)
             
         return templates.TemplateResponse("submitted.html", {"request": request, "text_input": text_input})
     
--- scraping_function.py
+++ scraping_function.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, re
+import requests
 
 # function that get the url input by the user and scrap the content of the page
 def scrap_url_user(url_input):
@@ -10,10 +10,11 @@ def scrap_url_user(url_input):
         soup = BeautifulSoup(page.content, 'html.parser')
         title_page = soup.title
         content = soup.get_text()
-        # content = re.sub(r'\n{2,}', '\n', str(content))
-        return content
-    else :
+        content = str(content)
+    else:
         print("Error when scraping ", str(url_input), " check the scrap_url_user function")
-
+        content = None
+    
+    return content
 
     # pas prendre en compte les \n ? ==> perte de notion de titre, contenu, ... 
